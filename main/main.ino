4f3c43d885523e00a7a9b39a23bc3fe30fd9992e
14FacundoCabrera <fcabrera100@hotmail.com>
2026-04-26 00:57:08 -0300

    Set signals to buttons added, improved buttons array to use std::vector

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,11 @@
 #include "IRremote.hpp"
+#include <vector>
 
 #define IR_RECEIVE_PIN 14 //D5
 #define IR_SENDER_PIN 4 //D2
 #define SET_IRSIGNALS_PIN 5 //D1
 #define BUTTON_1 0 //D3
-bool shouldExit = false; // to exit the reasign
+bool shouldExit = false; // to exit the reassign
 
 int pinButtons[] = { BUTTON_1 }; // todo: add more buttons
 
@@ -16,8 +17,7 @@ struct IrData { // It has the necessary data to send an IR signal
 
 struct IrDataButton { // The struct that holds the IrData, and assigns the data to a button pin
   int buttonPin;
-  IrData* data;
-  int length;
+  std::vector<IrData> data;
 };
 
 IrData button1Signals[] = {
@@ -26,9 +26,10 @@ IrData button1Signals[] = {
   {NEC, 0x820, 0x45}
 };
 
-IrDataButton buttons[] = {
-  {BUTTON_1, button1Signals, 3}
-};
+std::vector<IrDataButton> buttons;
+// IrDataButton buttons[] = {
+//   {BUTTON_1, button1Signals, 3}
+// };
 
 // IrData IrSignals[] = {
 //   {0x820, 0x1C},
@@ -37,14 +38,21 @@ IrDataButton buttons[] = {
 //   {0x820, 0x45}
 // };
 
+void buttonsSetup(){
+  for(int pin:pinButtons){
+    buttons.push_back({pin, {}});
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);  
   IrSender.begin(IR_SENDER_PIN);
   pinMode(SET_IRSIGNALS_PIN, INPUT_PULLUP);
+  buttonsSetup();
 }
 
-void setIrSignals(){
+void setIrSignals(int pin){
   uint16_t address = IrReceiver.decodedIRData.address;
   uint8_t command = IrReceiver.decodedIRData.command;
   auto protocol = IrReceiver.decodedIRData.protocol;
@@ -53,11 +61,17 @@ void setIrSignals(){
     IrReceiver.printIRResultShort(&Serial);
     Serial.println(address);
     Serial.println(command);
+    for(int i = 0; i < buttons.size(); i++){
+      if(pin == buttons[i].buttonPin){
+        buttons[i].data.push_back({protocol, address, command});
+        break;
+      }
+    }
   }
 }
 
 void sendIrSignals(IrData irData){
-  switch(irData.protocol){
+  switch(irData.protocol){ // case on IrData protocol
     case NEC:
       IrSender.sendNEC(irData.address, irData.command, 0);
       break;
@@ -67,16 +81,25 @@ void sendIrSignals(IrData irData){
   }
 }
 
+void deleteButtonData(int pin){
+  for(int i = 0; i < buttons.size(); i++){
+    if(pin == buttons[i].buttonPin){
+      buttons[i].data.clear(); // access the button data, then clears
+      break;
+    }
+  }
+}
+
 void buttonRegisterListener(int pin){
   if(digitalRead(pin) == LOW){ // to assign button pressed
-  // todo: delete already existing button pin data if exists
+    deleteButtonData(pin);
     while(!shouldExit){
       if (digitalRead(SET_IRSIGNALS_PIN) == LOW) { // Exit if pressed the main button
         shouldExit = true;
       } else {
         if(IrReceiver.decode()){
-          setIrSignals(); // todo: Set an IrData to the IrDataButton
-          IrReceiver.resume();
+          setIrSignals(pin);
+          IrReceiver.resume(); // resume IrReceiver to listen
         }
       }
     }
@@ -85,9 +108,9 @@ void buttonRegisterListener(int pin){
 
 void buttonSenderListener(int pin){
   if (digitalRead(pin) == LOW){
-    for(IrDataButton button : buttons){
+    for(IrDataButton &button : buttons){
       if(button.buttonPin == pin){
-        for(int i =0; i < button.length; i++){
+        for(int i =0; i < button.data.size(); i++){
           sendIrSignals(button.data[i]);
           delay(100); // todo: increase or decrease delay || maybe include a global delay var
         }
@@ -112,20 +135,7 @@ void loop() {
   }
    for(int pin : pinButtons){
     buttonSenderListener(pin);
-  } 
-  // if (IrReceiver.decode()) {
-  //   uint16_t address = IrReceiver.decodedIRData.address;
-  //   uint8_t command = IrReceiver.decodedIRData.command;
-  //   IrReceiver.printIRResultShort(&Serial);
-  //   Serial.println(address);
-  //   Serial.println(command);
-  //   IrSender.sendNEC(address, command, 3);
-  //   // for (int i = 0; i < 4; i++){
-  //   //   IrSender.sendNEC(IrSignals[i].address, IrSignals[i].command, 3);
-  //   //   delay(500); // todo: consider lower cooldown
-  //   // }
-  //   IrReceiver.resume();   
-  //}
+  }
 }
 // Power Off A: 0x820, C:0x0
 // One: A:0x820, C:0x1C
